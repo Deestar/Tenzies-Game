9c81a1ad28b34ad3296b14b7e01f47521d376042
Deestar <110073733+Deestar@users.noreply.github.com>
2023-02-02 06:26:20 -0800

    i created the function togen random nummber whenever button is clicked

--- react_file/src/components/main.js
+++ react_file/src/components/main.js
@@ -2,33 +2,27 @@ import React, { useEffect } from "react";
 import { Dice } from "./Dice";
 import { useState } from "react";
 export let Main = () => {
-  const [allArray, setAllArray] = useState([
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-  ]);
   let AllArrayNo = () => {
-    useEffect(
-      () =>
-        setAllArray(() =>
-          allArray.map(() => Math.floor(Math.random() * 6 + 1))
-        ),
-      []
-    );
-    return allArray.map((ele, ind) => <Dice key={ind + 1} value={ele} />);
+    let arr = [];
+    for (let index = 0; index < 10; index++) {
+      arr.push(Math.floor(Math.random() * 6 + 1));
+    }
+
+    return arr;
   };
-  console.log(AllArrayNo());
+  const [allArray, setAllArray] = useState(AllArrayNo());
+  let handleClick = () => {
+    setAllArray(() => allArray.map(() => Math.floor(Math.random() * 6 + 1)));
+  };
+
+  const Dies = () =>
+    allArray.map((ele, ind) => <Dice key={ind + 1} value={ele} />);
   return (
     <div className="main_cont">
-      <div className="btn_cont">{AllArrayNo()}</div>
-      <button className="roll">Roll</button>
+      <div className="btn_cont">{Dies()}</div>
+      <button onClick={handleClick} className="roll">
+        Roll
+      </button>
     </div>
   );
 };
