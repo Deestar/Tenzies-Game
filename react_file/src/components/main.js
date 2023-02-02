81d4fb9b5fce3a147c363f62bbe2927a839e3bda
Deestar <110073733+Deestar@users.noreply.github.com>
2023-02-02 05:32:06 -0800

    removed button

--- react_file/src/components/main.js
+++ react_file/src/components/main.js
@@ -27,10 +27,7 @@ export let Main = () => {
   console.log(AllArrayNo());
   return (
     <div className="main_cont">
-      <div className="btn_cont">
-        {AllArrayNo()}
-        <button>Roll</button>
-      </div>
+      <div className="btn_cont">{AllArrayNo()}</div>
     </div>
   );
 };
