37d89c01c91c2d2ef73b9c6e8a599e826909763e
Deestar <110073733+Deestar@users.noreply.github.com>
2023-02-07 08:50:18 -0800

    replaced unneccessary css code with react

--- react_file/src/components/Dice.js
+++ react_file/src/components/Dice.js
@@ -1,8 +1,22 @@
 import React from "react";
 export let Dices = (prop) => {
-  for (let index = 0; index < prop.value; index++) {}
+  const styles = prop.held
+    ? {
+        backgroundImage: `url(./css/img/img_${prop.value}.png)`,
+        filter: "invert(0.4)",
+        backgroundPosition: "center",
+        backgroundSize: "80%",
+        backgroundRepeat: "no-repeat",
+      }
+    : {
+        backgroundImage: `url(./css/img/img_${prop.value}.png)`,
+        backgroundPosition: "center",
+        backgroundSize: "80%",
+        backgroundRepeat: "no-repeat",
+      };
   return (
     <button
+      style={styles}
       className={prop.held ? `held_${prop.value}` : `img_${prop.value}`}
       onClick={() => {
         prop.handleHold(prop.id);
