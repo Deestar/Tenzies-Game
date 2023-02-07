e3ce0983d6747af5b6df77891883b0351b89df6f
Deestar <110073733+Deestar@users.noreply.github.com>
2023-02-07 08:10:58 -0800

    added dice

--- react_file/src/components/Dice.js
+++ react_file/src/components/Dice.js
@@ -1,13 +1,14 @@
 import React from "react";
 export let Dices = (prop) => {
+  for (let index = 0; index < prop.value; index++) {}
   return (
     <button
-      className={prop.held ? "held" : ""}
+      className={prop.held ? `held_${prop.value}` : `img_${prop.value}`}
       onClick={() => {
         prop.handleHold(prop.id);
       }}
     >
-      {prop.value}
+      {/* {prop.value} */}
     </button>
   );
 };
